1299a89609d90449602323e24ceec5906c5963ed
Mars <knownasmars@gmail.com>
2023-04-18 12:34:27 +0300

    1. Фильтры для объявлений машин [#4745]

--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -42,4 +42,8 @@ public class Post {
     @OneToOne
     @JoinColumn(name = "car_id")
     private Car car;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "post_id")
+    private List<File> files = new ArrayList<>();
 }
